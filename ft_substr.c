f79949e8598d540f683235942f89cc3effd0c506
Zakaria Gtaib <zgtaib@e2r9p12.1337.ma>
2023-11-14 20:10:48 +0100

    libft

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: zgtaib <zgtaib@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 20:21:19 by zgtaib            #+#    #+#             */
-/*   Updated: 2023/11/13 20:31:01 by zgtaib           ###   ########.fr       */
+/*   Updated: 2023/11/14 11:31:53 by zgtaib           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,20 @@ char	*ft_substr(char const *s, unsigned int start, size_t n)
 	size_t	x;
 	char	*str;
 
-	x = 0; 
-	if (n > ft_strlen(s))
-		n = ft_strlen(s);
-	str = (char *)malloc((n + 1) * sizeof(char));
-	if (!str)
-		return (0);
+	x = 0;
 	if (start >= ft_strlen(s) || n == 0 || !s)
 	{
+		str = malloc(sizeof(char));
+		if (!str)
+			return (0);
 		str[0] = '\0';
 		return (str);
 	}
+	if (n > ft_strlen(s + start))
+		n = ft_strlen(s + start);
+	str = (char *)malloc((n + 1) * sizeof(char));
+	if (!str)
+		return (0);
 	while (s[start] != '\0' && x < n)
 	{
 		str[x] = s[start];
